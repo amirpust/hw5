49d17da419f8279de366177f822487e0b99a2f24
amirpust <54210536+amirpust@users.noreply.github.com>
2021-06-28 12:02:34 +0300

    parserHPP

--- parser.hpp
+++ parser.hpp
@@ -8,10 +8,30 @@
 #include "RelopAUX.hpp"
 #include "Exp_t.hpp"
 #include "Table.hpp"
+#include "bp.hpp"
+#define codeBuffer (CodeBuffer::instance())
+
+extern symbolTable;
 
 class Parser{
 public:
+    Exp_t* ruleOP(Exp_t* exp1, Exp_t* exp2, string op){
 
+        if(!exp1->isNumerical() || !exp2->isNumerical()){
+            output::errorMismatch(yylineno);
+            output::printLog("isNumerical");
+            exit(12);
+        };
+        Exp_t* res = new Exp_t(Type(exp1->getDualType(*exp2)));
+
+        if(op != "/"){
+            codeBuffer.emitOp(res, exp1, op ,exp2);
+        }
+
+        delete exp1;
+        delete exp2;
+        return res;
+    }
 
 };
 
