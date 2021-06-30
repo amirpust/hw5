41b8c5196aeee5c4cd5fc605d2af7c407b60a9ac
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-06-30 14:00:28 +0300

    First

---
+++ cmake-build-debug/CMakeFiles/MyExecutable.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/BaseObj.cpp" "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/cmake-build-debug/CMakeFiles/MyExecutable.dir/BaseObj.cpp.obj"
+  "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/Enums.cpp" "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/cmake-build-debug/CMakeFiles/MyExecutable.dir/Enums.cpp.obj"
+  "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/Exp_t.cpp" "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/cmake-build-debug/CMakeFiles/MyExecutable.dir/Exp_t.cpp.obj"
+  "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/Symbol.cpp" "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/cmake-build-debug/CMakeFiles/MyExecutable.dir/Symbol.cpp.obj"
+  "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/bp.cpp" "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/cmake-build-debug/CMakeFiles/MyExecutable.dir/bp.cpp.obj"
+  "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/hw3_output.cpp" "C:/Users/jonat/OneDrive/Documents/Comp/HW5/hw5/cmake-build-debug/CMakeFiles/MyExecutable.dir/hw3_output.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/BaseObj.cpp.obj"
+  "CMakeFiles/MyExecutable.dir/Enums.cpp.obj"
+  "CMakeFiles/MyExecutable.dir/Exp_t.cpp.obj"
+  "CMakeFiles/MyExecutable.dir/Symbol.cpp.obj"
+  "CMakeFiles/MyExecutable.dir/bp.cpp.obj"
+  "CMakeFiles/MyExecutable.dir/hw3_output.cpp.obj"
+  "MyExecutable.exe"
+  "MyExecutable.exe.manifest"
+  "MyExecutable.pdb"
+  "libMyExecutable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
