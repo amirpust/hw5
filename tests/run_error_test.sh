91488a9a0171e379100e92a6a4ad9bfa44f7ca0d
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2021-06-30 14:50:23 +0300

    First

--- tests/run_error_test.sh
+++ tests/run_error_test.sh
@@ -2,6 +2,6 @@
 test_=$1
 
 echo "TEST te$test_"
-./hw5 < hw5-tests/te$test_.in 2>&1 > hw5-tests/te$test_.res
+../hw5 < hw5-tests/te$test_.in 2>&1 > hw5-tests/te$test_.res
 diff hw5-tests/te$test_.res hw5-tests/te$test_.out
 
